17db715d55305ec7e72184a8c2f486ec2ae974f5
Naza <141744770+Naza69@users.noreply.github.com>
2024-03-19 20:38:56 -0300

    Delete Ejercicios/Marzo directory

--- Ejercicios/Marzo/src/Vehicle.java
+++
@@ -1,8 +0,0 @@
-public abstract class Vehicle {
-    private int wheels;
-    private String colour;
-    public Vehicle(int wheels, String color){
-        this.wheels = wheels;
-        this.colour = colour;
-    }
-}
